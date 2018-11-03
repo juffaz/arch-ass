1a015eb6d7f1d19da21a38ee8ee69f6591282a92
Juffaz <38725561+juffaz@users.noreply.github.com>
2018-11-03 14:04:04 +0400

    Update arch-ass.sh

--- arch-ass.sh
+++ arch-ass.sh
@@ -119,7 +119,7 @@ EOF
 echo "LANG=en_GB.UTF-8" > /mnt/etc/locale.conf
 
 arch-chroot /mnt useradd -mU -s /usr/bin/zsh -G wheel,uucp,video,audio,storage,games,input "$user"
-arch-chroot /mnt chsh -s /usr/bin/bash
+arch-chroot /mnt chsh -s /usr/bin/zsh
 arch-chroot /mnt pacman -Sy nmap curl tcpdump xterm
 
 echo "$user:$password" | chpasswd --root /mnt
